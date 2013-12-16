1ab2ea3d7b327f8eb87ad6965b827128c50a1150
Shawn Rebello <shawn.nigel@gmail.com>
2013-12-16 18:36:43 +0530

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable, :confirmable,
+  devise :invitable, :database_authenticatable, :registerable, #:confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :tweets
 end
